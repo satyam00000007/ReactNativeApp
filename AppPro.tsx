5bf70e2d6027d5a7c7ebe394bfc6bec7fa32e677
Satyam Sharma <satyamshharish@gmail.com>
2025-05-11 10:34:04 +0530

    REACT-NATIVE Update text with color Schemes

---
+++ AppPro.tsx
@@ -0,0 +1,34 @@
+import React, { JSX } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme,
+} from 'react-native';
+
+function AppPro(): JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+                Hello World
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+    whiteText: {
+        color: '#FFFFFF',
+    },
+    darkText: {
+        color: '#000000',
+    },
+});
+
+export default AppPro;
